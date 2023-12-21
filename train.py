36f8e0f9aa9a1bf25403ad0dd72cf02b1d3ffa8e
dilyong <dlo26@cam.ac.uk>
2023-12-21 20:55:20 +0800

    Testing with different epochs input in .train() function.

--- train.py
+++ train.py
@@ -11,17 +11,19 @@ start_time = time.time()
 
 dir1 = 'bao.sdss_dr16'
 samples1 = read_chains(f"../ns/klcdm/{dir1}/{dir1}_polychord_raw/{dir1}") 
-fig, axes = make_2d_axes(['ns', 'omk', 'H0', 'ombh2', 'omch2', 'tau']) 
+#fig, axes = make_2d_axes(['ns', 'omk', 'H0', 'ombh2', 'omch2', 'tau']) 
+fig, axes = make_2d_axes(['omk', 'H0', 'ombh2', 'omch2']) 
 samples1.plot_2d(axes, label=f'{dir1}')
 
 
 #fig, axes = make_2d_axes(['omk', 'H0', 'omegam']) 
 #samples1.plot_2d(axes, label=f'{dir1}')
 #%%
-data = samples1.values[:, 1:7]
+data = samples1.values[:, 2:6]
 weights = samples1.get_weights()
 flow = MAF(data, weights=weights)
 flow.train(1000, early_stop=False)
+
 flow.save(f'{dir1}_flow.pkl')
 
 #%%
